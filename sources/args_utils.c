a5dc4a414ea5554a7fd6be5f3fb84a5837adf0b3
leroidubuffet <mr.bijou@gmail.com>
2023-04-04 14:59:14 +0200

    pointer check

--- sources/args_utils.c
+++ sources/args_utils.c
@@ -6,7 +6,7 @@
 /*   By: ybolivar <ybolivar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 16:26:59 by ybolivar          #+#    #+#             */
-/*   Updated: 2023/04/03 17:25:08 by ybolivar         ###   ########.fr       */
+/*   Updated: 2023/04/04 14:51:57 by ybolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_check_args(char **str, int argc, t_fractol *f)
 {
+	if (!f)
+		return (1);
 	if (ft_check_args_set(str, f))
 		return (1);
 	if (ft_check_args_coords(str, argc, f))
@@ -23,6 +25,8 @@ int	ft_check_args(char **str, int argc, t_fractol *f)
 
 int	ft_check_args_set(char **str, t_fractol *f)
 {
+	if (!f)
+		return (1);
 	if (ft_fractol_strcmp("Mandelbrot", str[1]))
 	{
 		ft_putstr("You chose Mandelbrot\n");
@@ -48,6 +52,8 @@ int	ft_check_args_set(char **str, t_fractol *f)
 
 int	ft_check_args_coords(char **str, int argc, t_fractol *f)
 {
+	if (!f)
+		return (1);
 	if (f->set == 2)
 	{
 		if (argc < 4)
